04564852e2763ab37896349e024dd4ea672b44a6
Sunoru <s@sunoru.com>
2021-08-06 01:00:20 -0400

    CI: update scripts.

--- .github/setup-jlnode.jl
+++ .github/setup-jlnode.jl
@@ -1,5 +1,5 @@
 using Pkg
-Pkg.add("JSON", "HTTP", "Downloads")
+Pkg.add.(["JSON", "HTTP", "Downloads"])
 using JSON, HTTP, Downloads
 Pkg.activate(".")
 Pkg.instantiate()
@@ -8,14 +8,13 @@ using libjlnode_jll
 libjlnode_jll.dev_jll()
 override_folder = libjlnode_jll.artifact_dir
 
-artifacts_info = String(
-    HTTP.request("GET", "https://api.github.com/repos/sunoru/jlnode/actions/artifacts")
-) |> JSON.parse
+response = HTTP.request("GET", "https://api.github.com/repos/sunoru/jlnode/actions/artifacts")
+artifacts_info = String(response.body) |> JSON.parse
 artifacts_info["total_count"] == 0 && exit(0)
 artifact = artifacts_info["artifacts"][1]
 artifact["expired"] && exit(0)
 url = artifact["archive_download_url"]
-output = download(url)
+output = Downloads.download(url)
 run(`7z x $output`)
 rm(joinpath(override_folder, "*"), recursive=true)
 mv("lib", override_folder)
